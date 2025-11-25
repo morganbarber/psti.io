8f3a098ce9137eccabffd434add97299fd16b0aa
Morgan Barber <morgan@morganbarber.me>
2025-11-24 19:39:07 -0700

    feat: Add E2E tests for pastes and configure Jest with a new setup file.

---
+++ apps/api/test/jest-setup.ts
@@ -0,0 +1,3 @@
+process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://example.supabase.co';
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = 'example-key';
+process.env.SUPABASE_SERVICE_ROLE_KEY = 'example-service-role-key';
